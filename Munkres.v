26acf2cd00b39c0488569844295ab31ccaa919ae
Matthew Piziak <matthew.piziak@gmail.com>
2019-05-01 18:46:54 -0400

    having fun with inclusion

--- Munkres.v
+++ Munkres.v
@@ -1,24 +1,11 @@
-Require Export TopologicalSpaces.
-Require Export InteriorsClosures.
-From ZornsLemma Require Export EnsemblesSpec.
+Require Export Coq.Sets.Ensembles.
+Require Export Coq.Sets.Powerset.
 
 Section Chapter_1.
   Variable U : Type.
 
-Theorem exercise_1_1_2: forall A B C : Ensemble U,
-    Intersection A (Union B C) = (Union (Intersection A B) (Intersection A C)).
-Proof.
-    intros A B C.
-    apply Extensionality_Ensembles.
-    split; red; intros x H'.
-    elim H'.
-    intros x0 H'0 H'1; generalize H'0.
-    elim H'1; auto with sets.
-    elim H'; intros x0 H'0; elim H'0; auto with sets.
-Qed.
-
 Theorem exercise_1_1_1: forall A B C : Ensemble U,
-    Union A (Intersection B C) = (Intersection (Union A B) (Union A C)).
+    Union U A (Intersection U B C) = (Intersection U (Union U A B) (Union U A C)).
 Proof.
     intros A B C.
     apply Extensionality_Ensembles.
@@ -32,4 +19,31 @@ Proof.
     elim H'2; auto with sets.
 Qed.
 
-End Chapter_1.
+Theorem exercise_1_1_2: forall A B C : Ensemble U,
+    Intersection U A (Union U B C) = (Union U (Intersection U A B) (Intersection U A C)).
+Proof.
+    intros A B C.
+    apply Extensionality_Ensembles.
+    split; red; intros x H'.
+    elim H'.
+    intros x0 H'0 H'1; generalize H'0.
+    elim H'1; auto with sets.
+    elim H'; intros x0 H'0; elim H'0; auto with sets.
+Qed.
+
+(* Theorem exercise_1_1_3: forall A B C : Ensemble U, *)
+(*     Setminus A (Union B C) = Intersection (Setminus A B) (Setminus A C). *)
+(* Proof.*)
+
+Theorem exercise_1_2_1: forall A B C : Ensemble U,
+    Included U A B /\ Included U A C <-> Included U A (Intersection U B C).
+Proof.
+  intros.
+  split.
+  intros.
+  destruct H.
+  apply Intersection_maximal; assumption.
+  intros.
+  split.
+
+(* End Chapter_1. *)
