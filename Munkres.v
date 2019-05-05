4f97d4c1268693b0d7c7d9c6ee505cd5fe2ee7df
Matthew Piziak <matthew.piziak@gmail.com>
2019-05-05 17:36:42 -0400

    more effort in set theory

--- Munkres.v
+++ Munkres.v
@@ -32,7 +32,49 @@ Proof.
     elim H'; intros x0 H'0; elim H'0; auto with sets.
 Qed.
 
-Theorem exercise_1_2_1: forall A B C : Ensemble U,
+Theorem exercise_1_2_a_forward: forall A B C : Ensemble U,
+    Included U A B /\ Included U A C -> Included U A (Union U B C).
+Proof.
+  intros.
+  destruct H.
+  unfold Included.
+  intros.
+  auto with sets.
+Qed.
+
+(* Lemma swap {X} (P: X -> Prop):  (exists x, ~ P x) -> ~ forall x, P x. *)
+(* Proof. intros [x HPx] H. apply HPx, H. Qed. *)
+
+(* Theorem exercise_1_2_a_backward: exists A B C : Ensemble U, *)
+(*     ~ (Included U A (Union U B C) -> Included U A B /\ Included U A C). *)
+(* Proof. *)
+(*   unfold not. *)
+
+(* Theorem exercise_1_2_b: forall A B C : Ensemble U, *)
+(*     Included U A B \/ Included U A C <-> Included U A (Union U B C). *)
+(* Proof. *)
+(*   intros. *)
+(*   split. *)
+(*   - intros. *)
+(*     destruct H. *)
+(*     * unfold Included. *)
+(*       unfold Included in H. *)
+(*       intros. *)
+(*       apply H in H0. *)
+(*       unfold In. *)
+(*       left. *)
+(*       assumption. *)
+(*     * unfold Included. *)
+(*       unfold Included in H. *)
+(*       intros. *)
+(*       apply H in H0. *)
+(*       unfold In. *)
+(*       right. *)
+(*       assumption. *)
+(*   - intros. *)
+
+
+Theorem exercise_1_2_c: forall A B C : Ensemble U,
     Included U A B /\ Included U A C <-> Included U A (Intersection U B C).
 Proof.
   intros.
@@ -59,6 +101,5 @@ Proof.
   assumption.
 Qed.
 
-End Chapter_1.
 
-Locate "_ * _".
+End Chapter_1.
